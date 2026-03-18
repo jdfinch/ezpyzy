91e8bd4efc18607d0d927db74f2246b221e0c80f
James Finch <jdifnch@emory.edu>
2026-03-17 20:44:33 -0400

    Filling in methods

--- src/ezpyzy/table.py
+++ src/ezpyzy/table.py
@@ -36,18 +36,33 @@ class Table(typing.Generic[T]):
             assert len(ids) == len(self), f"Hash collision created by new id_function"
             self.ids = ids
             self._id_function = id_function
-
+    
     def add(self, item: T):
+        """Add an item to the Table, removing any existing items with its ID, at a given index"""
         if self._id_function:
             key = self._id_function(item)
             if key not in self.ids:
+                self.ids[key] = len(self)
                 self.rows.append(item)
-            self.ids[key] = item
+            else:
+                pass
         else:
             self.rows.append(item)
-        return self
+
+    def insert(self, item: T, index: int):
+        """Add an item to the Table, if its ID does not already exist in the table"""
+        if self._id_function:
+            key = self._id_function(item)
+            assert key not in self.ids, f"Item with ID {key} already exists."
+            self.ids[key] = index
+            for i in range(index, len(self.rows)):
+                self.ids[self._id_function(self.rows[i])] += 1
+            self.rows.insert(index, item)
+        else:
+            self.rows.insert(item, index)    
 
     def append(self, item: T):
+        """Add an item to the Table, erroring if an item that already has its ID exists."""
         if self._id_function:
             key = self._id_function(item)
             assert key not in self.ids, f"Item with ID {key} already exists."
@@ -56,6 +71,7 @@ class Table(typing.Generic[T]):
         return self
 
     def extend(self, items: typing.Iterable[T]):
+        """Add items to the Table, erroring if an item that already has one of their IDs exists."""
         if self._id_function:
             keys = [self._id_function(item) for item in items]
             for key in keys:
@@ -65,11 +81,12 @@ class Table(typing.Generic[T]):
         return self
 
     def update(self, items: typing.Iterable[T]):
+        """Add items to the Table, ignoring items whose IDs are already in the Table"""
         if self._id_function:
             keys = (self._id_function(item) for item in items)
             for key, item in zip(keys, items):
                 if key not in self.ids:
-                    self.ids[key] = item
+                    self.ids[key] = len(self.rows)
                     self.rows.append(item)
         else:
             self.rows.extend(items)
@@ -81,7 +98,8 @@ class Table(typing.Generic[T]):
     def __len__(self):
         return len(self.rows)
 
-    def tryget(self, getter: typing.Callable[[T], A]) -> 'Table[A]':
+    def try_get(self, getter: typing.Callable[[T], A]) -> 'Table[A]':
+        """Produce a new Table by attempting to apply a predicate over each row of this table"""
         result = Table()
         for row in self:
             try:
@@ -102,30 +120,25 @@ class Table(typing.Generic[T]):
         return result
     
     def get(self, getter: typing.Callable[[T], E]) -> 'Table[E]':
-        result = self.__class__(getter(item) for item in self)
-        return result
+        """Produce a new Table by applying a predicate over each row of this table"""
+        gotten = self.__class__()
+        gotten.extend(getter(item) for item in self)
+        return gotten
 
-    def filter(self, by: typing.Callable[[T], bool]|typing.Sequence[bool] = None) -> 'Table[T]':
-        if by is None:
-            by = self
-        elif callable(by):
-            by = [by(x) for x in self]
-        filter = self.__class__((item for item, flag in zip(self, by) if flag))
-        return filter
+    def filter(
+        self,
+        by: typing.Callable[[T], bool]|typing.Sequence[bool]|dict|set = None
+    ) -> 'Table[T]':
+        """Filter rows to leave only those with a True predicate, or with IDs in the dict/set"""
     
-    def select(self, keys: typing.Sequence) -> 'Table[T]':
-        selection = self.__class__(self.ids[key] for key in keys)
-        return selection
-    
-    def lookup(self, indices: typing.Sequence[int]) -> 'Table[T]':
-        gathered = self.__class__(self[index] for index in indices)
-        return gathered
+    def select(self, ids: typing.Collection) -> 'Table[T]':
+        """Produce a new Table from this Table's rows by ids"""
     
     def group(self, by: typing.Callable[[T], G]) -> dict[G, 'Table[T]']:
         groups = {}
         for item in self:
             key = by(item)
-            groups.setdefault(key, self.__class__()).append(item)
+            groups.setdefault(key, self.__class__(key=self._id_function)).append(item)
         return groups
 
     def sort(self, by: typing.Callable[[T], typing.Any]|typing.Iterable, reverse=False):
@@ -133,6 +146,7 @@ class Table(typing.Generic[T]):
             items_and_keys = [(item, key) for item, key in zip(self, by)]
             assert len(items_and_keys) == len(self), f"Sort by sequence must be the same length as the Table"
             items_and_keys.sort(key=lambda x: x[1], reverse=reverse)
+            self.rows = [item for item, key in items_and_keys]
         else:
             self.rows.sort(key=by, reverse=reverse)
         return self
@@ -176,14 +190,13 @@ if __name__ == '__main__':
             return 5
 
     a = Foo(['hello', 'world'])
-    b = Foo(['world'])
+    b = Foo(['world', 'war', 'peace'])
     c = Foo(['h', 'w'], a)
 
     table = Table((a, b, c)) 
     print(f"{list(table.get(lambda x: x.bar[0])) = }")
     print(f"{list(table.get(lambda x: x.bar[1].capitalize())) = }")
     print(f"{list(table.get(lambda x: x.bat)) = }")
-    print(f"{list(table.get(lambda x: x.bat())) = }")
 
 
 
